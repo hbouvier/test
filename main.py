a2a9cc95126c8e75f92e3f61cb99f841e9eb7d6e
Henri Bouvier <henribouvier@yahoo.com>
2016-07-28 13:31:30 -0400

    resolved conflict m3 and a1

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 allo
 added this on master
 first commit on assignment
+Arman A2
 added this on master (M2) after the A1 --> M3
 first commit on assignment
 erdem A2 commit before arman (but after M3 that was not merged) 
