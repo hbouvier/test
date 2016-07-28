6db910fb5cb27e1259d34ef25282e415a74b044c
Henri Bouvier <henribouvier@yahoo.com>
2016-07-28 13:23:26 -0400

    A1

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 allo
 added this on master
+first commit on assignment
 added this on master (M2) after the A1 --> M3
 first commit on assignment
 erdem A2 commit before arman (but after M3 that was not merged) 
